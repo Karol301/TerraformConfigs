8a6f407f213f9d0b283216b4d28bdcf27539c6ec
Karol Arabasz <karolarabasz6@gmail.com>
2025-08-04 22:46:09 +0200

    Terraform backend, lock + state file

--- Terraform_backend/backend.tf
+++ Terraform_backend/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mybucket"
-    key    = "path/to/my/key"
+    bucket = "Karol301-bucket"
+    key    = "myproject/terraform.tfstate"
     region = "eu-north-1"
     dynamodb_table = "terraform-lock"
   }
